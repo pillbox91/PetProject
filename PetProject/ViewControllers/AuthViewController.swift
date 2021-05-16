6eefc3159a935c1fdd526fdec326b0e194de003f
Andrey Averyanov <averyanov1996@gmail.com>
2021-05-16 09:36:01 +0300

    Initial Commit

---
+++ PetProject/ViewControllers/AuthViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AuthViewController.swift
+//  PetProject
+//
+//  Created by Андрей Аверьянов on 16.05.2021.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordtexxtField: UITextField!
+    @IBOutlet var logInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
