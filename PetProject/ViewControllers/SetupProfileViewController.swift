093ce7e380144c78ce31cc3d8b1b4bbed2eb3f5f
Andrey Averyanov <averyanov1996@gmail.com>
2021-08-04 21:59:04 +0300

    1

---
+++ PetProject/ViewControllers/SetupProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SetupProfileViewController.swift
+//  PetProject
+//
+//  Created by Андрей Аверьянов on 04.08.2021.
+//
+
+import UIKit
+
+class SetupProfileViewController: UIViewController {
+    
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var addPhotoBytton: UIButton!
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var surnameTextField: UITextField!
+    
+    @IBOutlet var genderSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var chatButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        chatButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func goTochatButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
