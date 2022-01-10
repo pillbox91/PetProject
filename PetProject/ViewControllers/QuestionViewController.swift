c120f232f8839649f5c0a12b15feb16b409c461b
Andrey Averyanov <averyanov1996@gmail.com>
2022-01-10 13:59:37 +0300

    Start app

---
+++ PetProject/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionViewController.swift
+//  PetProject
+//
+//  Created by Андрей Аверьянов on 10.01.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
